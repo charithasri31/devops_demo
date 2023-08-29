aa4e085f3ce139d5c87134bf7e6b6a230328a467
charithasri31 <charithasridondapati31@gmail.com>
2023-08-29 15:15:25 +0530

    second commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to Dhanekula...!" );
+        System.out.println( "Welcome to Dhanekula...! This is workshop session." );
     }
 }
